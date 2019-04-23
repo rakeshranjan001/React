94be8a6b39b030aa0ee5f1c3e0fc5a105d07efd6
Rakesh Ranjan <rranjan@GSHYD-C1MRQ3CBDTY3.local>
2019-04-23 12:25:59 +0530

    Home Page Done

---
+++ build/precache-manifest.becd2c1ac55196fb61546b098eac5772.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "8be184ead932aaea6a24",
+    "url": "/static/css/main.84c5e0b9.chunk.css"
+  },
+  {
+    "revision": "8be184ead932aaea6a24",
+    "url": "/static/js/main.29e147b9.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.c5541365.js"
+  },
+  {
+    "revision": "10534ad094e56cc863c6",
+    "url": "/static/js/2.4b44abd2.chunk.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "89e555b759c84a8e31a1a6a41a705317",
+    "url": "/index.html"
+  }
+];
